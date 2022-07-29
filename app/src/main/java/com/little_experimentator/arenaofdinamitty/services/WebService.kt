f30774b6ebd889a0c99f03ade72e20a7fd1e6ccb
besumniy <besumniy@gmail.com>
2022-07-29 17:54:34 +0300

    Try to use service for web

--- app/src/main/java/com/little_experimentator/arenaofdinamitty/services/WebService.kt
+++ app/src/main/java/com/little_experimentator/arenaofdinamitty/services/WebService.kt
@@ -6,7 +6,21 @@ import android.os.IBinder
 
 class WebService : Service() {
 
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onCreate(){
+        //make connection
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //close socket
     }
 }
